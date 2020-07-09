95465cd85b9660dadb89dd9e04cc7e911551a983
Dan Brown <dsjbrown@icloud.com>
2020-07-09 08:56:15 +0100

    Added mergeData()

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Data Science, Getting and Cleaning Data Week 4 Assignment
 This ReadMe contains descriptions of the how all the scripts work and how they are connected.
 
 ## Scripts
-1. importData()
+### importData()
 A function to import the relevant files from your working directory.  
-It assumes the files are contained in a sub folder called 'UCI HAR Dataset' and stored in a manner identical to the folder downloaded for the project.
+It assumes the files are contained in a sub folder called 'UCI HAR Dataset' and stored in a manner identical to the folder downloaded for the project.  
+It creates three data frames:
+1. dTest: contains the content of 'X_test.txt'
+2. dTrain: contains the content of 'X-train.txt'
+3. dLabels: contains the contents of 'features.txt'
+
+### mergeData()
+A function which merges the dTest and dTrain datasets into a new combined data frame.  Variable labels are applied, using the content of dLabels, then all variables are removed, other than those containing mean or standard deviation measures.
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,57 @@
-## Function to import 
+## -------------------------------------------------------------
+## importData()
+## Function to import run analysis data
+## Folder 'UCI HAR Dataset' must be a peer of the project folder
+## -------------------------------------------------------------
+
 importData <- function() {
+        # Remember current working directory (project folder)
+        vWd = getwd()
+        # Skip up a level
+        setwd("..")
+        # Drop down into the folder containing the datasets
+        setwd("./UCI HAR Dataset")
         
-              
+        # Read in the test dataset and assign to an object 
+        # in the global environment
+        dTest <<- read.table("test/X_test.txt", header = FALSE, dec = ".", 
+                            comment.char = "", colClasses = "numeric")
+        
+        # Read in the train dataset and assign to an object 
+        # in the global environment        
+        dTrain <<- read.table("train/X_train.txt", header = FALSE, dec = ".", 
+                            comment.char = "", colClasses = "numeric")
+        
+        # Read in the variable labels and assign to an object 
+        # in the global environment
+        dLabels <<- read.table("features.txt", header = FALSE, comment.char = "")
+
+        # Revert working directory to the project folder
+        setwd(vWd)
+
 }
 
+## --------------------------------------------
+## mergeData()
 ## Function to merge the training and test sets 
 ## to create one data set
-mergeData <- function(tName) {
+## --------------------------------------------
+
+mergeData <- function() {
         
+        ## Join the two tables
+        dAllData <- rbind(dTest, dTrain)
+        ## Add variable labels using the dLabels object
+        names(dAllData) = dLabels[ ,2]
+        ## Return the results to the global environment 
+        dCombine <<- dAllData
+        # Tidy up the global objects that were used in this function
+        rm(dTest, envir = .GlobalEnv)
+        rm(dTrain, envir = .GlobalEnv)      
+        rm(dLabels, envir = .GlobalEnv)  
+
 }
+
+# Set labels on the test and train datasets
+names(dTest) = dLabels[ ,2]
+names(dTrain) = dLabels[ ,2]
